43eda53a08b9b0fb56a21c0c82d51f553bbaaf33
Shukla <s.rahulelite@gmail.com>
2024-12-19 23:33:58 +0530

    totalAmountspendbyCustomer

--- min-temperatures.py
+++ min-temperatures.py
@@ -10,12 +10,19 @@ def parseLine(line):
     temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
     return (stationID, entryType, temperature)
 
-lines = sc.textFile("file:///SparkCourse/1800.csv")
+lines = sc.textFile("1800.csv")
 parsedLines = lines.map(parseLine)
+# For Minimum Temperature
 minTemps = parsedLines.filter(lambda x: "TMIN" in x[1])
 stationTemps = minTemps.map(lambda x: (x[0], x[2]))
 minTemps = stationTemps.reduceByKey(lambda x, y: min(x,y))
 results = minTemps.collect();
+
+# For maximum Temperature
+# maxTemps = parsedLines.filter(lambda x: "TMAX" in x[1])
+# stationTemps = maxTemps.map(lambda x: (x[0], x[2]))
+# maxTemps = stationTemps.reduceByKey(lambda x, y: max(x,y))
+# results = maxTemps.collect();
 
 for result in results:
     print(result[0] + "\t{:.2f}F".format(result[1]))
